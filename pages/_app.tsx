8bb591be34465055685447c50a5bff311d12983a
James Howat <97859901+jbhowat@users.noreply.github.com>
2023-01-05 08:50:13 -0700

    small fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
